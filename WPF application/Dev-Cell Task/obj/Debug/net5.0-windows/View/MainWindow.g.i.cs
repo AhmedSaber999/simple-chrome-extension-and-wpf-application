39a97abf6e97ec7a77521f3d02f8de4973e65739
Ahmed Saber <A7mdsaber99@gmail.com>
2021-08-06 15:47:25 +0200

    rename desktop folder to WPF

--- WPF application/Dev-Cell Task/obj/Debug/net5.0-windows/View/MainWindow.g.i.cs
+++ WPF application/Dev-Cell Task/obj/Debug/net5.0-windows/View/MainWindow.g.i.cs
@@ -76,7 +76,7 @@ namespace Dev_Cell_Task {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Dev-Cell Task;component/view/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Dev-Cell Task;V1.0.0.0;component/view/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
